daf2dbe242f8b99232476e12fd522f8b3b5ea4c1
Bulat Khabirov <khabiroff_b@me.com>
2018-05-18 13:22:08 +0300

    Initial Commit

---
+++ J2ObjC-quadratic-equations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  J2ObjC-quadratic-equations
+//
+//  Created by Bulat Khabirov on 18.05.2018.
+//  Copyright © 2018 Bulat Khabirov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
